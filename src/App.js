9489e7d2363bdee64b73c7d8ae064090bfe65051
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2018-12-11 10:19:23 -0500

    Static state and empty page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    books: [
+      {
+        "title": "The Linux Command Line",
+        "authors": [
+          "William E. Shotts, Jr."
+        ], "id": "nggnmAEACAAJ",
+        "shelf": "currentlyReading",
+        "imageLinks": {
+          "smallThumbnail": "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        },
+      },
+      {
+        "title": "Learning Web Development with React and Bootstrap",
+        "authors": [
+          "Harmeet Singh",
+          "Mehul Bhatt"
+        ], "id": "sJf1vQAACAAJ",
+        "shelf": "currentlyReading",
+        "imageLinks": {
+          "smallThumbnail": "http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=sJf1vQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        },
+      },
+      {
+        "title": "The Cuckoo's Calling",
+        "authors": [
+          "Robert Galbraith"
+        ],
+        "imageLinks": {
+          "smallThumbnail": "http://books.google.com/books/content?id=evuwdDLfAyYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=evuwdDLfAyYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        },
+        "id": "evuwdDLfAyYC",
+        "shelf": "wantToRead"
+      }
+    ]
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="body">
+        <header className="list-books-title">
+          <h1>my-reads</h1>
         </header>
       </div>
     );
