3fa7e2a0a6854b2a899ecb3d5af0a723bd6f5433
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-01-04 13:29:52 -0500

    Routes for list and search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import Bookshelf from './Bookshelf';
+import React, { Component } from "react";
+import "./App.css";
+import Bookshelf from "./Bookshelf";
+import Search from "./Search";
+import { Route } from "react-router-dom";
 
 class App extends Component {
-
   render() {
     return (
       <div className="body">
         <header className="list-books-title">
           <h1>my-reads</h1>
         </header>
-        <div className='list-books-content'>
-          <Bookshelf className='bookshelf' />
-        </div>
+
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books-content">
+              <Bookshelf className="bookshelf" />
+            </div>
+          )}
+        />
+
+        <Route exact path="/search" component={Search} />
       </div>
     );
   }
---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+  render() {
+    return (
+      <div className='search-books-bar'>
+        Search Bar
+      </div>
+    )
+  }
+}
+
+export default Search
