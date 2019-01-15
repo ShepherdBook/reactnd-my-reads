c5baba73c7214fd2ea5673bcc1e0d9c22eb9aefa
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-01-15 11:23:29 -0500

    Use map for shelves instead of calling BookList three times

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -5,9 +5,21 @@ import { Link } from "react-router-dom";
 class Bookshelf extends Component {
   render() {
     const { books, handleChange } = this.props;
-    const currentlyReadingBooks = books.filter(book => book.shelf === "currentlyReading");
-    const wantToReadBooks = books.filter(book => book.shelf === "wantToRead");
-    const readBooks = books.filter(book => book.shelf === "read");
+
+    const shelves = [];
+
+    shelves.push({
+      title: "Currently Reading",
+      books: books.filter(book => book.shelf === "currentlyReading")
+    });
+    shelves.push({
+      title: "Want To Read",
+      books: books.filter(book => book.shelf === "wantToRead")
+    });
+    shelves.push({
+      title: "Read",
+      books: books.filter(book => book.shelf === "read")
+    });
 
     return (
       <div>
@@ -15,9 +27,7 @@ class Bookshelf extends Component {
           <h1>my-reads</h1>
         </header>
 
-        <BookList title={"Currently Reading"} books={currentlyReadingBooks} handleChange={handleChange} />
-        <BookList title={"Want to Read"} books={wantToReadBooks} handleChange={handleChange} />
-        <BookList title={"Read"} books={readBooks} handleChange={handleChange} />
+        {shelves.map((shelf, i) => <BookList key={i} title={shelf.title} books={shelf.books} handleChange={handleChange} />)}
 
         <Link to="/search" className="open-search">
           <button>Open Search</button>
