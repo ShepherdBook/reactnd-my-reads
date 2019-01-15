c3a9d76d6035f539e53203d5575468acb4cbfb44
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-01-15 11:27:42 -0500

    Make BookList a stateless component

--- src/BookList.js
+++ src/BookList.js
@@ -1,25 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import Book from "./Book";
 
-class BookList extends Component {
-  render() {
-    const { title, books, handleChange } = this.props;
-
-    return (
-      <div>
-        <header className="bookshelf-title">
-          <h2>{title}</h2>
-        </header>
-        <ul className="books-grid">
-          {books.length > 0 && books.map(book => (
+const BookList = (props) => {
+  return (
+    <div>
+      <header className="bookshelf-title">
+        <h2>{props.title}</h2>
+      </header>
+      <ul className="books-grid">
+        {props.books.length > 0 &&
+          props.books.map(book => (
             <li key={book.id}>
-              <Book book={book} handleChange={handleChange} />
+              <Book book={book} handleChange={props.handleChange} />
             </li>
           ))}
-        </ul>
-      </div>
-    );
-  }
-}
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
